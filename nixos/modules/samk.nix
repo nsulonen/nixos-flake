6a56481227264facc7c95e48de325a7623926e12
nsulonen <niko.sulonen@proton.me>
2025-09-02 17:25:19 +0300

    enable openvpn3

--- nixos/modules/samk.nix
+++ nixos/modules/samk.nix
@@ -1,20 +1,9 @@
 { ... }:
 
 {
-  networking.networkmanager.ensureProfiles = {
-    "SAMK-STUDENT" = {
-      type = "wifi";
-      interface-name = "wlp4s0";
-      autoconnect = true;
-      ssid = "SAMK-STUDENT";
-      wifi-sec.key-mgmt = "wpa-eap";
-      "802-1x".eap = "peap";
-      "801-1x".identity = "sa32999";
-      "802-1x".phase2-auth = "mschapv2";
-      "802-1x".domain = "samk.fi";
-      "802-1x".altsubject-matches = "radius-student.samk.fi";
-      "802-1x".system-ca-certs = true;
-      "802-1x".password-flags = 1;
+  programs = {
+    openvpn3 = {
+      enable = true;
     };
   };
 }
