261212b87b5c6097b8bdacdecab548a680771703
nsulonen <niko.sulonen@proton.me>
2025-09-03 19:40:47 +0300

    add attribute

--- vpn/flake.nix
+++ vpn/flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     {
       devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
         buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.openvpn3 ];
