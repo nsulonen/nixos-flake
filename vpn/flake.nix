a23df84e9019dd6ea9cb1994eea3e3903adb07d7
nsulonen <niko.sulonen@proton.me>
2025-09-03 19:29:24 +0300

    studentvpn for samk

---
+++ vpn/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "openvpn3 from nixos-25.05";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.openvpn3 ];
+      };
+    };
+}
