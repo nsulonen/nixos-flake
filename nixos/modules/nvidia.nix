ec9f490a0eb506e4421a650e93d6beb4e918bbeb
nsulonen <niko.sulonen@proton.me>
2025-09-13 11:30:58 +0300

    enable wlr portal for nvidia screencasting

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,15 +1,29 @@
 { config, ... }:
 
 {
-    hardware.nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
 
-        open = true;
+    open = true;
 
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  #enable wlr portal for nvidia screencasting
+  xdg.portal = {
+    enable = true;
+
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          force_mod_linear = true;
+        };
+      };
     };
-
-    services.xserver.videoDrivers = [ "nvidia" ];
+  };
 }
