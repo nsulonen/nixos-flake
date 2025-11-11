b1553ac7cf0852ee47375ee6a6d6875cc78bbbfd
nsulonen <niko.sulonen@proton.me>
2025-11-11 14:47:48 +0200

    round corners and add borders

--- home-manager/modules/configs/niri/default.nix
+++ home-manager/modules/configs/niri/default.nix
@@ -77,11 +77,11 @@
       focus-ring.enable = false;
 
       border = {
-        enable = false;
+        enable = true;
         width = 1.0;
       };
 
-      shadow.enable = false;
+      shadow.enable = true;
     };
 
     spawn-at-startup = [
@@ -118,10 +118,10 @@
     window-rules = [
       {
         geometry-corner-radius = {
-          top-left = 0.0;
-          top-right = 0.0;
-          bottom-right = 0.0;
-          bottom-left = 0.0;
+          top-left = 8.0;
+          top-right = 8.0;
+          bottom-right = 8.0;
+          bottom-left = 8.0;
         };
         clip-to-geometry = true;
       }
