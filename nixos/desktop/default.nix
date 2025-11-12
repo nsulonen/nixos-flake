3ef7e0787d7fcc808a68f3f876c9a4db8cbeede9
nsulonen <niko.sulonen@proton.me>
2025-11-12 18:00:44 +0200

    disable udev wake rule

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -16,4 +16,8 @@
       enableGraphical = true;
     };
   };
+
+  services.udev.extraRules = ''
+    ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x43ee" ATTR{power/wakeup}="disabled"
+  '';
 }
