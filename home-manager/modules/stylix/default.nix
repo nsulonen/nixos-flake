1f3f26fe3fcafd9ba375504e7668d05d43d83c71
nsulonen <niko.sulonen@proton.me>
2026-01-07 12:39:23 +0200

    refactor: move stylix fonts and cursor config to system module

--- home-manager/modules/stylix/default.nix
+++ home-manager/modules/stylix/default.nix
@@ -6,28 +6,6 @@
     zen-browser.profileNames = [ "default" ];
   };
 
-  stylix = {
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Original-Ice";
-      size = 36;
-    };
-    fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font Mono";
-      };
-      sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
-      };
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
-    };
-  };
-
   gtk = {
     enable = true;
     iconTheme = {
