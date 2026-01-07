e94711f6f3a654a56689dea9791af1bec1ce96dc
nsulonen <niko.sulonen@proton.me>
2026-01-07 12:49:24 +0200

    cleaning up

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows ="home-manager";
     };
 
     zen-browser = {
